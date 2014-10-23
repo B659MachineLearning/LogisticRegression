1ea097aeff73f50ca414d9ba2d056cad834fd428
mtare <mtare@indiana.edu>
2014-10-22 22:50:17 -0400

    Generalised LR

--- src/com/machinelearning/LogisticRegression/LogisticRegression.java
+++ src/com/machinelearning/LogisticRegression/LogisticRegression.java
@@ -8,30 +8,45 @@ public class LogisticRegression {
 	static double step = Double.parseDouble(Config.readConfig("stepSize"));
 	private static int numOfRuns = Integer.parseInt(Config.readConfig("iterations"));
 	
+	public static String label = null;
+	public static int labelIndex;
+	public static String trueClassLabel = "1";
+	
 	private static double sigmoid(double z){
 	        return 1 / (1 + Math.exp(-z)); 
 	}
 	
 	public static double classifier(Example instance){
 		double linearSum = 0.0;
-		for (int j = 0; j<DataLoader.numberOfFeatures-1;j++){
-			linearSum += weights[j] * Double.parseDouble(instance.getFeature(j));
+		for (int j = 0; j<DataLoader.numberOfFeatures; j++){
+			if(j != labelIndex)
+				linearSum += weights[j] * Double.parseDouble(instance.getFeature(j));
 		}
 		return sigmoid(linearSum);
 	}
 	
 	public static void parameterComputation(ArrayList<Example> ex){
 		for (int n = 0; n < numOfRuns; n++){
+			double checkDiff = 0.0;
 			for (int i=0; i < ex.size(); i++){
 					double predictedValue = classifier(ex.get(i));
-					String classLabel = ex.get(i).getFeature(16);
-					if(Integer.parseInt(classLabel) != 1 )
+					String classLabel = ex.get(i).getFeature(labelIndex);
+					if(!classLabel.equalsIgnoreCase(trueClassLabel))
 						classLabel = "0";
-					for (int j = 0;j<DataLoader.numberOfFeatures-1;j++){
-						weights[j]= weights[j] + step * (Double.parseDouble(classLabel) - predictedValue) * Double.parseDouble(ex.get(i).getFeature(j));
+					for (int j = 0; j<DataLoader.numberOfFeatures; j++){
+						if(j != labelIndex){
+							weights[j]= weights[j] + step * (Double.parseDouble(classLabel) - predictedValue) * Double.parseDouble(ex.get(i).getFeature(j));
+						}
 					}
-				//	for (int k =0;k<weights.length;k++)
-					//System.out.println("\nWieght for"+"  " + i  +"is  "+" "+weights[k]);
+					for (int k =0;k<weights.length;k++)
+						System.out.print("\nWieght for"+"  " + i  +"is  "+" "+weights[k]);
+					System.out.println();
+			}
+			checkDiff = 1;
+			System.out.println("Difference : "+checkDiff);
+			if(checkDiff < 0.01){
+				System.out.println("Iterations Completed : "+n);
+				break;
 			}
 		}        
 	}
@@ -51,7 +66,11 @@ public class LogisticRegression {
 		ArrayList<Example> trainExamples = DataLoader.readRecords(trainFilePath);
 		ArrayList<Example> testExamples = DataLoader.readRecords(testFilePath);
 		
-		weights = new double [DataLoader.numberOfFeatures - 1];
+		//Index of the class label
+		label = Config.readConfig("classLable");
+		labelIndex = DataLoader.labels.indexOf(label);
+				
+		weights = new double [DataLoader.numberOfFeatures];
 		//System.out.println(weights);
 		
 		
@@ -70,20 +89,21 @@ public class LogisticRegression {
 		
 		parameterComputation(trainExamples);
 		
-		/*for(int l=0; l<weights.length;l++)
-			System.out.println(weights[l]);*/
+//		for(int l=0; l<weights.length;l++)
+//			System.out.println(weights[l]);
 		
 		for(int e = 0; e < testExamples.size();e++){
 			
 			predictedClass1 =  classifier(testExamples.get(e));
 			predictedClass0 = 1 - predictedClass1;
+			
 			if(predictedClass0 > predictedClass1)
 				predictedClassLabel = 0;
 			else
 				predictedClassLabel = 1;
 			
-			System.out.println(predictedClassLabel);
-			if(Integer.parseInt(testExamples.get(e).getFeature(16)) != 1)
+			System.out.println("prediction : "+predictedClassLabel+" actual : "+testExamples.get(e).getFeature(labelIndex));
+			if(Integer.parseInt(testExamples.get(e).getFeature(labelIndex)) != 1)
 				actualClassLabel = 0;
 			else
 				actualClassLabel = 1;
